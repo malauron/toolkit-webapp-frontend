be85492c40ddba4bffd75aefbe24b7fd7af6f455
Mark Lauron <malauron@gmail.com>
2020-07-07 17:36:19 +0800

    Hide Navigation Button

--- src/app/layout/side-nav/side-nav.component.ts
+++ src/app/layout/side-nav/side-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SideNavService } from '../../services/side-nav.service';
 
 @Component({
   selector: 'app-side-nav',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public sideNavService: SideNavService) { }
 
   ngOnInit(): void {
   }
--- src/app/layout/top-nav/top-nav.component.ts
+++ src/app/layout/top-nav/top-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SideNavService } from '../../services/side-nav.service';
 
 @Component({
   selector: 'app-top-nav',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public sideNavService: SideNavService) { }
 
   ngOnInit(): void {
   }
