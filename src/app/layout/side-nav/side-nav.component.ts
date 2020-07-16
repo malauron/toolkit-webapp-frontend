b941b29481dc738e5eab7c0a5d294f39e8b66681
Mark Lauron <malauron@gmail.com>
2020-07-16 10:04:08 +0800

    Update Style

--- src/app/layout/side-nav/side-nav.component.ts
+++ src/app/layout/side-nav/side-nav.component.ts
@@ -4,6 +4,8 @@ import { ChangeDetectionStrategy,
   OnDestroy,
   Input } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { NavigationService } from '../../services/navigation.service';
+import { SideNavItems, SideNavSection } from '../../models/navigation.models';
 
 @Component({
   selector: 'app-side-nav',
@@ -13,13 +15,13 @@ import { Subscription } from 'rxjs';
 })
 export class SideNavComponent implements OnInit, OnDestroy {
     @Input() sidenavStyle!: string;
-    // @Input() sideNavItems!: SideNavItems;
-    // @Input() sideNavSections!: SideNavSection[];
+    @Input() sideNavItems!: SideNavItems;
+    @Input() sideNavSections!: SideNavSection[];
 
     subscription: Subscription = new Subscription();
     routeDataSubscription!: Subscription;
 
-    // constructor(public navigationService: NavigationService, public userService: UserService) {}
+    constructor(public navigationService: NavigationService) {}
 
     ngOnInit() {}
 
